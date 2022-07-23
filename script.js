8d6ea6162c702bc87c806f58cf0c4224e3e8659a
Sean Dillon <pdillon0@gmail.com>
2022-07-23 17:13:26 -0400

    correct

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
+// Question and answer lists
 var questionList = ['What is a document method Query Selector?'];
 var answerListOne = ['Returns the first element within the document that matches the the specified selector(s)'];
 var answerListTwo = ['Returns the number of child elements of the document.'];
 var answerListThree = ['A special variable which can hold more than one value.'];
 var answerListFour = ['Allows you to break up your code into separate fields.'];
 
-var correctAnswer = false;
 var questionNumber = 0;
 
 var quiz = document.querySelector('.container');
@@ -16,15 +15,24 @@ var three = document.querySelector('.answer3').textContent = answerListThree[que
 var four = document.querySelector('.answer4').textContent = answerListFour[questionNumber];
 // var result =document.querySelector('.result');
 
-var button =document.querySelector('button');
+// var button =document.querySelector('button');
 var result = document.querySelector('.result');
 var answers = document.querySelector('.answers');
+var correctAnswer = document.querySelector('.answer1');
 
 answers.addEventListener('click', (event) => {
-    var isButton = event.target.nodeName === 'BUTTON';
-    if (isButton) {
-  result.classList.remove('show');
-  void result.offsetWidth;
-  result.classList.add('show');
+    var element = event.target
+    if (element.matches('button') ) {
+        if (element.matches('.answer1')) {
+       result.classList.remove('show');
+       void result.offsetWidth;
+       result.classList.add('show');
+       result.textContent = 'Correct!';
+        } else {
+            result.classList.remove('show');
+            void result.offsetWidth;
+            result.classList.add('show'); 
+            result.textContent = 'That answer is incorrect!';
+        };
     };
 });
